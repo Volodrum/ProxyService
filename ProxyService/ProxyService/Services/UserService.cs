7a659ec3481d6e7f2f039abe44d2eeb20c11a4fa
Volodrum <volodimirshakula@gmail.com>
2024-09-15 12:51:54 +0300

    UserService changes

--- ProxyService/ProxyService/Services/UserService.cs
+++ ProxyService/ProxyService/Services/UserService.cs
@@ -21,17 +21,12 @@ namespace ProxyService.Services
             // Перевіряємо, чи є користувач в кеші
             if (_userCache.ContainsKey(id))
             {
-                Log.Information("User with ID {Id} found in cache", id);
                 return _userCache[id];
             }
 
-            // Якщо користувача немає в кеші, виконуємо HTTP-запит до reqres.in
-            // Логування запиту до зовнішнього API
-            Log.Information("Fetching user with ID {Id} from external API", id);
             var response = await _httpClient.GetAsync($"https://reqres.in/api/users/{id}");
             if (!response.IsSuccessStatusCode)
             {
-                Log.Warning("User with ID {Id} not found in external API", id);
                 return null; // повертаємо null, якщо користувача не знайдено
             }
 
@@ -40,11 +35,6 @@ namespace ProxyService.Services
 
             //var userResponse = JsonSerializer.Deserialize<ReqresUserResponse>(responseContent);
 
-
-
-            // Додаємо користувача в кеш
-            // Логування додавання користувача в кеш
-            Log.Information("Adding user with ID {Id} to cache", id);
             _userCache[id] = userResponse.Data;
 
             return userResponse.Data;
